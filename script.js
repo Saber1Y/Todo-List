8957ec30bbab13f82d0e59f405c108c0a21afed1
ayodeji <ayodejiakintobi1@gmail.com>
2023-09-20 11:39:03 +0100

    added removal function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const inputBox = document.getElementById('inputBox');
 const list = document.getElementById('list');
-const listItem = document.getElementById('listItem');
 const addBtn = document.querySelector('.addBtn');
 
-addBtn.addEventListener('click', () => {
+
+addBtn.addEventListener('click', function(e) {
   addTask();
+//     const deleteBtn = e.target;
+
+//   if (deleteBtn.classList.contains('deleteButton')) {
+//     //trying to access my List itmes using parantNode (have never used)//
+//        deleteBtn.style.display = 'none';
+//   }
 });
 
 function addTask() {
@@ -12,14 +18,19 @@ function addTask() {
 
     if( addedList !== "") {
         const listTodo = document.createElement('li');
-        // listTodo.textContent = addedList;
+        listTodo.textContent = addedList;
         listTodo.innerHTML = `<input type="checkBox"> ${addedList}`;
         list.appendChild(listTodo);
         inputBox.value = "";
-
+      
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'X';
         deleteButton.classList.add('delete-button');
         listTodo.appendChild(deleteButton);
-    }
-}
+
+        deleteButton.addEventListener('click', ()=> {
+            // deleteButton.classList.remove('listTodo');
+            listTodo.style.display = 'none';
+        });
+    } 
+};
